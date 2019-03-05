ee70138ffa0e3d19384869a6e2084b8e1eb17280
dislersd <dislersd@gmail.com>
2019-03-05 13:34:14 -0800

    completed addtodo action creator and action - can add todo tasks to list

--- todo/src/components/ToDoForm.js
+++ todo/src/components/ToDoForm.js
@@ -1,30 +1,47 @@
 import React from "react";
+import { connect } from "react-redux";
+import { addToDo } from "../actions";
 
 class ToDoForm extends React.Component {
   state = {
-    todo: ''
-  }
+    todo: ""
+  };
 
   handleChange = e => {
     this.setState({
       [e.target.name]: e.target.value
-    })
-  }
+    });
+  };
+
+  addToDo = e => {
+    e.preventDefault();
+    this.props.addToDo(this.state.todo);
+    this.setState({ todo: "" });
+  };
 
   render() {
     return (
-      <div className='form'>
-        <input 
-        type="text"
-        name="todo"
-        value={this.state.value}
-        onChange={this.handleChange}
-        placeholder='add a new todo...'  
+      <div className="form">
+        <input
+          type="text"
+          name="todo"
+          value={this.state.todo}
+          onChange={this.handleChange}
+          placeholder="add a new todo..."
         />
-        <button> + </button>
+        <button onClick={this.addToDo}> + </button>
       </div>
     );
   }
 }
 
-export default ToDoForm;
+const mapStateToProps = state => {
+  return {
+    todo: state.todos
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { addToDo }
+)(ToDoForm);
--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,5 +1,4 @@
-import {ADD_TODO} from '../actions'
-
+import { ADD_TODO } from "../actions";
 
 const initialState = {
   todos: [
@@ -12,16 +11,18 @@ const reducer = (state = initialState, action) => {
   console.log(action);
   switch (action.type) {
     case ADD_TODO:
-    const newToDo = {
-      task: action.payload,
-      id: Date.now(),
-      completed: false
-    };
-    return {
-      todos: [...state.todos, newToDo]
-    }
+      const newToDo = {
+        task: action.payload,
+        id: Date.now(),
+        completed: false
+      };
+      return {
+        ...state,
+        todos: [...state.todos, newToDo]
+      };
+    default:
+      return state;
   }
-  return state;
 };
 
 export default reducer;
